8d0a7b5627d0db85ac6de7cfae5e7b1991f9642b
ruooooooli@gmail.com <ruooooooli@gmail.com>
2016-01-19 18:48:19 +0800

    xxx

--- string.go
+++ string.go
@@ -3,7 +3,7 @@
 * @Email                : ruooooooli@gmail.com
 * @Date                 : 2016-01-19 18:26:46
 * @Last Modified by     : ruoli
-* @Last Modified time   : 2016-01-19 18:34:29
+* @Last Modified time   : 2016-01-19 18:47:43
  */
 package utils
 
@@ -11,12 +11,23 @@ import (
 	"crypto/md5"
 	"fmt"
 	"io"
+	"math/rand"
 )
 
 func MD5(str string) string {
 	hashMD5 := md5.New()
 	io.WriteString(hashMD5, str)
 	return fmt.Sprintf("%x", hashMD5.Sum(nil))
+}
+
+func RandomString(length int) string {
+	rand.Seed(time.Now().UnixNano())
+	runes := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = runes[rand.Intn(len(runes))]
+	}
+	return string(b)
 }
 
 /* End of file  : string.go */
