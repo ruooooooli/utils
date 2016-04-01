ac03505e578f48e0f2491806c8504c7d81922ffd
ruooooooli <ruooooooli@gmail.com>
2016-04-01 14:35:50 +0800

    add some changes

--- string.go
+++ string.go
@@ -1,10 +1,11 @@
 /*
 * @Author               : ruoli
 * @Email                : ruooooooli@gmail.com
-* @Date                 : 2016-01-19 18:26:46
-* @Last Modified by     : ruoli
-* @Last Modified time   : 2016-01-19 18:49:05
+* @Date                 : 2016-03-30 11:50:02
+* @Last Modified by     : ruooooooli
+* @Last Modified time   : 2016-04-01 14:35:27
  */
+
 package utils
 
 import (
@@ -15,21 +16,32 @@ import (
 	"time"
 )
 
-func MD5(str string) string {
-	hashMD5 := md5.New()
-	io.WriteString(hashMD5, str)
-	return fmt.Sprintf("%x", hashMD5.Sum(nil))
+const (
+	chars         = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	letterIdxBits = 6
+	letterIdxMask = 1<<letterIdxBits - 1
+	letterIdxMax  = 63 / letterIdxBits
+)
+
+func MD5(data string) string {
+	h := md5.New()
+	io.WriteString(h, data)
+	return fmt.Sprintf("%x", h.Sum(nil))
 }
 
 func RandomString(length int) string {
-	rand.Seed(time.Now().UnixNano())
-	runes := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-	b := make([]rune, length)
-	for i := range b {
-		b[i] = runes[rand.Intn(len(runes))]
+	src := rand.NewSource(time.Now().UnixNano())
+	b := make([]byte, length)
+	for i, cache, remain := length-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(chars) {
+			b[i] = chars[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
 	}
 	return string(b)
 }
-
-/* End of file  : string.go */
-/* Location     : /string.go */
